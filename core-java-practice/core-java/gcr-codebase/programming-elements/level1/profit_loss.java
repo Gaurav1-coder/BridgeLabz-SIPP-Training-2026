d0ba1f43ff5972ac1e174aef0cf05502f536afc9
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-06-16 10:11:24 +0530

    [Gaurav] Added: Profit_lose problem

---
+++ core-java-practice/core-java/gcr-codebase/programming-elements/level1/profit_loss.java
@@ -0,0 +1,9 @@
+public class profit_loss {
+    public static void main(String[] args) {
+        int cp=129;
+        int sp=191;
+        int profit=sp-cp;
+        double Percentage = profit / cp * 100;
+        System.out.print(Percentage);
+    }
+}
