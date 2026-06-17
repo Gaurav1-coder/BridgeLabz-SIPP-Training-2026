42f62bb87083a5588c8519b8265f00581dcda394
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-06-17 13:00:14 +0530

    [Gaurav] Added: PalindromeRecursion Problem

---
+++ core-java-practice/core-java/gcr-codebase/recursion/level1/PalindromeRecursion.java
@@ -0,0 +1,24 @@
+public class PalindromeRecursion {
+
+    public static boolean isPalindrome(String str, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+
+        if (str.charAt(left) != str.charAt(right)) {
+            return false;
+        }
+
+        return isPalindrome(str, left + 1, right - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+
+        if (isPalindrome(str, 0, str.length() - 1)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
