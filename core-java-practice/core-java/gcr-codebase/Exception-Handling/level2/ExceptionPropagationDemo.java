a4d10d844755d75cab476ea335b572b9d72ebf96
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-06-17 13:18:31 +0530

    [Gaurav] Added: ExceptionPropagationDemo Problem

---
+++ core-java-practice/core-java/gcr-codebase/Exception-Handling/level2/ExceptionPropagationDemo.java
@@ -0,0 +1,27 @@
+public class ExceptionPropagationDemo {
+
+    // Method 1: Generates exception
+    public static void method1() {
+
+        int result = 10 / 0; // ArithmeticException
+    }
+
+    // Method 2: Calls method1
+    public static void method2() {
+
+        method1();
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            // Calls method2
+            method2();
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Handled exception in main");
+        }
+    }
+}
