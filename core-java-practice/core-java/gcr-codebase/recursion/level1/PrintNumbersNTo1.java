cfcf9a13d301e09b1c565266b5ca74ced917a22d
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-06-17 12:59:08 +0530

    [Gaurav] Added: PrintNumbersNTo1 Problem

---
+++ core-java-practice/core-java/gcr-codebase/recursion/level1/PrintNumbersNTo1.java
@@ -0,0 +1,16 @@
+public class PrintNumbersNTo1 {
+
+    public static void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        System.out.print(n + " ");
+        printNumbers(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        printNumbers(n);
+    }
+}
