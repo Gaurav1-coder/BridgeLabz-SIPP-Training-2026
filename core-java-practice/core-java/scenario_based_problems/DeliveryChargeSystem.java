b7637c5ee4b20a3645483eac98ac2db6fa2eb0f2
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-06-17 13:25:19 +0530

    [Gaurav] Added: DeliveryChargeSystem Problem

---
+++ core-java-practice/core-java/scenario_based_problems/DeliveryChargeSystem.java
@@ -0,0 +1,45 @@
+public class DeliveryChargeSystem {
+
+    // Distance only
+    public static double calculateCharge(int distance) {
+
+        return distance * 5;
+    }
+
+    // Distance + Weight
+    public static double calculateCharge(int distance,
+                                         int weight) {
+
+        return (distance * 5) + (weight * 10);
+    }
+
+    // Distance + Weight + Express Delivery
+    public static double calculateCharge(int distance,
+                                         int weight,
+                                         boolean expressDelivery) {
+
+        double charge =
+                (distance * 5) + (weight * 10);
+
+        if (expressDelivery) {
+            charge += 100;
+        }
+
+        return charge;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Charge 1: ₹" +
+                calculateCharge(20));
+
+        System.out.println(
+                "Charge 2: ₹" +
+                calculateCharge(20, 5));
+
+        System.out.println(
+                "Charge 3: ₹" +
+                calculateCharge(20, 5, true));
+    }
+}
