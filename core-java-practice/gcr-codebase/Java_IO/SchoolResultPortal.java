3fb9da202312b28eb0beadbbf58fc404381a2c1a
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-06-23 12:52:31 +0530

    [Gauravv] Added Java IO Assignment SchoolR esultPortal.java

---
+++ core-java-practice/gcr-codebase/Java_IO/SchoolResultPortal.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class SchoolResultPortal {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader br =
+                    new BufferedReader(new FileReader("students.txt"));
+
+            PrintWriter pw =
+                    new PrintWriter(new FileWriter("report.txt", true));
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                String name = data[0];
+
+                int m1 = Integer.parseInt(data[1]);
+                int m2 = Integer.parseInt(data[2]);
+                int m3 = Integer.parseInt(data[3]);
+
+                double avg = (m1 + m2 + m3) / 3.0;
+
+                pw.println("Name : " + name);
+                pw.println("Average : " + avg);
+                pw.println("--------------------");
+            }
+
+            br.close();
+            pw.close();
+
+            System.out.println("Report generated successfully.");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Student file not found.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
