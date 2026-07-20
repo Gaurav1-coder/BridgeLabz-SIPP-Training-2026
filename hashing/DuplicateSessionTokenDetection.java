94bde29f1ad266c46866f9e1ee1368fc624b6cbf
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-07-20 09:44:06 +0530

    [Gauravv] Added: DuplicateSessionTokenDetection

---
+++ hashing/DuplicateSessionTokenDetection.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class DuplicateSessionTokenDetection {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        HashSet<String> set = new HashSet<>();
+
+        boolean duplicate = false;
+
+        for (int i = 0; i < n; i++) {
+            String token = sc.next();
+
+            if (set.contains(token)) {
+                duplicate = true;
+                break;
+            }
+
+            set.add(token);
+        }
+
+        System.out.println(duplicate);
+    }
+}
