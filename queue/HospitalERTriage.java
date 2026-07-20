9dfca0b4d86a3fb09e347fe1f50b39b4d44956cc
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-07-20 09:36:00 +0530

    [Gauravv] Added: HospitalERTriage

---
+++ queue/HospitalERTriage.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class HospitalERTriage {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.offer(4);
+        pq.offer(1);
+        pq.offer(5);
+        pq.offer(2);
+        pq.offer(3);
+
+        while (!pq.isEmpty()) {
+            System.out.println("Treat Patient with Priority: " + pq.poll());
+        }
+    }
+}
