132c1d776def16a2a5b8a1137998611d6549f0b1
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-07-20 09:45:47 +0530

    [Gauravv] Added: FraudPairDetection

---
+++ hashing/FraudPairDetection.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class FraudPairDetection {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int target = sc.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                System.out.println("Indices: " + map.get(complement) + " " + i);
+                return;
+            }
+
+            map.put(arr[i], i);
+        }
+
+        System.out.println("No pair found");
+    }
+}
