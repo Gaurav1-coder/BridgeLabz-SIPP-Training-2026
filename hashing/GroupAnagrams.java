e8fb90ddb2c2955274983417f8f2ce5e18c93d5f
Gaurav Katiyar <183537767+Gaurav1-coder@users.noreply.github.com>
2026-07-20 09:44:52 +0530

    [Gauravv] Added: GroupAnagrams

---
+++ hashing/GroupAnagrams.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String[] strs = new String[n];
+
+        for (int i = 0; i < n; i++)
+            strs[i] = sc.next();
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+
+            String key = new String(ch);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        for (List<String> group : map.values()) {
+            System.out.println(group);
+        }
+    }
+}
